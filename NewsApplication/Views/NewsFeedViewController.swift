595a4655ffdb42483a0a81419d98665f6c79b508
webdmitriev <webdmitriev@gmail.com>
2024-12-09 01:01:38 +0300

    Search bar NewsFeedViewController - is ready

--- NewsApplication/Views/NewsFeedViewController.swift
+++ NewsApplication/Views/NewsFeedViewController.swift
@@ -10,25 +10,18 @@ import UIKit
 class NewsFeedViewController: UIViewController {
     
     var mok: [NewsEntity] = [
-            NewsEntity(thumbnail: "mokphoto1", title: "This man spent $15,000 making a Tesla Cybertruck out of wood", date: "13.12.24", urlText: "https://mail.ru", descr: "The wooden Cybertruck replica includes LED lights and a X logo. ND - WoodArt A Vietnamese man built a fully functional replica of Tesla's Cybertruck out of wood. Truong Van Dao's been driving his family around in the car, which is being sent to Elon Musk, The wooden Cybertruck replica includes LED lights and a X logo. ND - WoodArt A Vietnamese man built a fully functional replica of Tesla's Cybertruck out of wood. Truong Van Dao's been driving his family around in the car, which is being sent to Elon Musk. The wooden Cybertruck replica includes LED lights and a X logo. ND - WoodArt A Vietnamese man built a fully functional replica of Tesla's Cybertruck out of wood. Truong Van Dao's been driving his family around in the car, which is being sent to Elon Musk "),
-            NewsEntity(thumbnail: "mokphoto2", title: "Huawei to move smart car operations to new joint company with Changan", date: "13.11.24", urlText: "www.mail.ru/asdfasdf/asdfasdf/asdfasdf/asdfasdfasdfasdf/asdfasdf/asdfasdf", descr: "China's Huawei said on Sunday it will move core technologies and resources in its smart car unit, which has chalked up robust sales for a number of new vehicles, to a new joint company owned up to 40% by automaker Changan Auto. The new company will engage in"),    NewsEntity(thumbnail: "mokphoto1", title: "This man spent $15,000 making a Tesla Cybertruck out of wood", date: "13.12.24", urlText: "businessinsider.com", descr: "The wooden Cybertruck replica includes LED lights and a X logo. ND - WoodArt A Vietnamese man built a fully functional replica of Tesla's Cybertruck out of wood. Truong Van Dao's been driving his family around in the car, which is being sent to Elon Musk"),
+            NewsEntity(thumbnail: "mokphoto1", title: "Oleg man spent $15,000 making a Tesla Cybertruck out of wood", date: "13.12.24", urlText: "https://mail.ru", descr: "The wooden Cybertruck replica includes LED lights and a X logo. ND - WoodArt A Vietnamese man built a fully functional replica of Tesla's Cybertruck out of wood. Truong Van Dao's been driving his family around in the car, which is being sent to Elon Musk, The wooden Cybertruck replica includes LED lights and a X logo. ND - WoodArt A Vietnamese man built a fully functional replica of Tesla's Cybertruck out of wood. Truong Van Dao's been driving his family around in the car, which is being sent to Elon Musk. The wooden Cybertruck replica includes LED lights and a X logo. ND - WoodArt A Vietnamese man built a fully functional replica of Tesla's Cybertruck out of wood. Truong Van Dao's been driving his family around in the car, which is being sent to Elon Musk "),
+            NewsEntity(thumbnail: "mokphoto2", title: "Vadim to move smart car operations to new joint company with Changan", date: "13.11.24", urlText: "www.mail.ru/asdfasdf/asdfasdf/asdfasdf/asdfasdfasdfasdf/asdfasdf/asdfasdf", descr: "China's Huawei said on Sunday it will move core technologies and resources in its smart car unit, which has chalked up robust sales for a number of new vehicles, to a new joint company owned up to 40% by automaker Changan Auto. The new company will engage in"),    NewsEntity(thumbnail: "mokphoto1", title: "This man spent $15,000 making a Tesla Cybertruck out of wood", date: "13.12.24", urlText: "businessinsider.com", descr: "The wooden Cybertruck replica includes LED lights and a X logo. ND - WoodArt A Vietnamese man built a fully functional replica of Tesla's Cybertruck out of wood. Truong Van Dao's been driving his family around in the car, which is being sent to Elon Musk"),
             NewsEntity(thumbnail: "mokphoto2", title: "Huawei to move smart car operations to new joint company with Changan", date: "13.11.24", urlText: "www.mail.ru", descr: "China's Huawei said on Sunday it will move core technologies and resources in its smart car unit, which has chalked up robust sales for a number of new vehicles, to a new joint company owned up to 40% by automaker Changan Auto. The new company will engage in"),    NewsEntity(thumbnail: "mokphoto1", title: "This man spent $15,000 making a Tesla Cybertruck out of wood", date: "13.12.24", urlText: "businessinsider.com", descr: "The wooden Cybertruck replica includes LED lights and a X logo. ND - WoodArt A Vietnamese man built a fully functional replica of Tesla's Cybertruck out of wood. Truong Van Dao's been driving his family around in the car, which is being sent to Elon Musk"),
             NewsEntity(thumbnail: "mokphoto2", title: "Huawei to move smart car operations to new joint company with Changan", date: "13.11.24", urlText: "www.mail.ru", descr: "China's Huawei said on Sunday it will move core technologies and resources in its smart car unit, which has chalked up robust sales for a number of new vehicles, to a new joint company owned up to 40% by automaker Changan Auto. The new company will engage in"),    NewsEntity(thumbnail: "mokphoto1", title: "This man spent $15,000 making a Tesla Cybertruck out of wood", date: "13.12.24", urlText: "businessinsider.com", descr: "The wooden Cybertruck replica includes LED lights and a X logo. ND - WoodArt A Vietnamese man built a fully functional replica of Tesla's Cybertruck out of wood. Truong Van Dao's been driving his family around in the car, which is being sent to Elon Musk"),
             NewsEntity(thumbnail: "mokphoto2", title: "Huawei to move smart car operations to new joint company with Changan", date: "13.11.24", urlText: "www.mail.ru", descr: "China's Huawei said on Sunday it will move core technologies and resources in its smart car unit, which has chalked up robust sales for a number of new vehicles, to a new joint company owned up to 40% by automaker Changan Auto. The new company will engage in"),    NewsEntity(thumbnail: "mokphoto1", title: "This man spent $15,000 making a Tesla Cybertruck out of wood", date: "13.12.24", urlText: "businessinsider.com", descr: "The wooden Cybertruck replica includes LED lights and a X logo. ND - WoodArt A Vietnamese man built a fully functional replica of Tesla's Cybertruck out of wood. Truong Van Dao's been driving his family around in the car, which is being sent to Elon Musk"),
             NewsEntity(thumbnail: "mokphoto2", title: "Huawei to move smart car operations to new joint company with Changan", date: "13.11.24", urlText: "www.mail.ru", descr: "China's Huawei said on Sunday it will move core technologies and resources in its smart car unit, which has chalked up robust sales for a number of new vehicles, to a new joint company owned up to 40% by automaker Changan Auto. The new company will engage in")
             ]
-        
-//    lazy var scrollView: UIScrollView = {
-//        $0.backgroundColor = .clear
-//        $0.addSubview(scrollContent)
-//        return $0
-//    }(UIScrollView(frame: view.frame))
     
-//    lazy var scrollContent: UIView = {
-//        $0.translatesAutoresizingMaskIntoConstraints = false
-//        $0.addSubview(tableView)
-//        return $0
-//    }(UIView())
+    var searchedMok = [NewsEntity]()
+
+    var isSearching = false
+    var searchBar = UISearchBar()
     
     private lazy var tableView: UITableView = {
         $0.dataSource = self
@@ -45,6 +38,16 @@ class NewsFeedViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         
         view.addSubviews(tableView)
+
+        searchBar.delegate = self
+        searchBar.sizeToFit()
+        searchBar.searchBarStyle = .prominent
+        searchBar.placeholder = "Поиск новости VK..."
+        searchBar.tintColor = UIColor.appGrayText
+        searchBar.barTintColor = UIColor.appBlack
+
+        navigationItem.titleView = searchBar
+        navigationController?.hidesBarsOnSwipe = true
         
         setupConstraints()
     }
@@ -59,19 +62,57 @@ class NewsFeedViewController: UIViewController {
 
         ])
     }
+    
+    // MARK: Search filter
+//    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+//        let text: String = self.searchBar.text ?? ""
+//        self.filteredNews = mok.filter { $0.title.contains(text) }
+//        
+//        self.tableView.reloadData()
+//        self.searchBar.delegate = self
+//        
+//    }
 }
 
-extension NewsFeedViewController: UITableViewDataSource , UITableViewDelegate {
+//// MARK: UISearchBarDelegate
+//extension NewsFeedViewController: UISearchBarDelegate {
+//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+//        guard let text = searchBar.text else { return }
+//        searchBar.resignFirstResponder()
+//        mok.removeAll()
+//    }
+//}
+//
+//// MARK:
+//extension NewsFeedViewController: UISearchResultsUpdating {
+//    func updateSearchResults(for searchController: UISearchController) {
+//        guard let text = searchController.searchBar.text else { return }
+//    }
+//}
+
+
+extension NewsFeedViewController: UITableViewDataSource , UITableViewDelegate, UISearchBarDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        mok.count
+        if isSearching {
+             return searchedMok.count
+         } else {
+             return mok.count
+         }
     }
     
 //    Конфигурация ячейки
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let item = mok[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: NewsCell.reuseId, for: indexPath) as! NewsCell
-        cell.setupView(item: item)
-        cell.selectionStyle = .none
+
+        if isSearching {
+            let item = searchedMok[indexPath.row]
+            cell.setupView(item: item)
+            cell.selectionStyle = .none
+        } else {
+            let item = mok[indexPath.row]
+            cell.setupView(item: item)
+            cell.selectionStyle = .none
+        }
         return cell
     }
     
@@ -86,5 +127,20 @@ extension NewsFeedViewController: UITableViewDataSource , UITableViewDelegate {
         
         navigationController?.pushViewController(vc, animated: true)
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        searchedMok = mok.filter({
+            $0.title.lowercased().prefix(searchText.count) == searchText.lowercased()
+//            $0.title.lowercased().prefixMatch(of: searchText.lowercased()) == searchText.lowercased()
+        })
+        isSearching = true
+        tableView.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.isSearching = false
+        searchBar.text = ""
+        tableView.reloadData()
+     }
 }
 
